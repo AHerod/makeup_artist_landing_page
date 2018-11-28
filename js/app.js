b02ea86e4e9e11e779913ad6a559ecfa81b3dd65
AHerod <metalvege@gmail.com>
2018-11-28 18:45:45 +0100

    sass files structure import

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
-
-import "./scss/style.scss";
-
+import "./scss/abstracts/_mixins.scss";
+import "./scss/abstracts/_module.scss";
+import "./scss/abstracts/_variables.scss";
+import "./scss/base/_animations.scss";
+import "./scss/base/_base.scss";
+import "./scss/base/_typography.scss";
+import "./scss/components/_aboutCard.scss";
+import "./scss/components/_button.scss";
+import "./scss/components/_form.scss";
+import "./scss/layout/_footer.scss";
+import "./scss/layout/_grid.scss";
+import "./scss/layout/_header.scss";
+import "./scss/layout/_navigation.scss";
 
