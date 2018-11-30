72cb4855b0b2f141bcee47107a20d776333af847
AHerod <metalvege@gmail.com>
2018-11-30 14:04:46 +0100

    header RWD part 2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,6 @@
 import "./scss/abstracts/_mixins.scss";
 import "./scss/abstracts/_module.scss";
 import "./scss/abstracts/_variables.scss";
-import "./scss/base/_animations.scss";
-import "./scss/base/_base.scss";
-import "./scss/base/_typo.scss";
-import "./scss/components/_aboutCard.scss";
-import "./scss/components/_buttons.scss";
-import "./scss/components/_form.scss";
 import "./scss/layout/_footer.scss";
 import "./scss/layout/_grid.scss";
 import "./scss/layout/_header.scss";
@@ -14,4 +8,11 @@ import "./scss/layout/_navigation.scss";
 import  "./scss/layout/_about.scss";
 import  "./scss/layout/_contact.scss";
 import "./scss/layout/_gallery.scss";
+import "./scss/base/_animations.scss";
+import "./scss/base/_base.scss";
+import "./scss/base/_typo.scss";
+import "./scss/components/_aboutCard.scss";
+import "./scss/components/_buttons.scss";
+import "./scss/components/_form.scss";
+
 
