7d56d22305307f0ed5cc90dd1eef6d8f1774d0a3
AHerod <metalvege@gmail.com>
2018-11-30 19:07:22 +0100

    social icons added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-import "./scss/abstracts/_mixins.scss";
+import "./scss/abstracts/_mixxins.scss";
 import "./scss/abstracts/_module.scss";
 import "./scss/abstracts/_variables.scss";
 import "./scss/layout/_footer.scss";
 import "./scss/layout/_gallery.scss";
 import "./scss/layout/_header.scss";
 import "./scss/layout/_navigation.scss";
-import  "./scss/layout/_about.scss";
-import  "./scss/layout/_contact.scss";
+import "./scss/layout/_about.scss";
+import "./scss/layout/_contact.scss";
 import "./scss/layout/_gallery.scss";
 import "./scss/base/_animations.scss";
 import "./scss/base/_base.scss";
@@ -14,10 +14,26 @@ import "./scss/base/_typo.scss";
 import "./scss/components/_aboutCard.scss";
 import "./scss/components/_buttons.scss";
 import "./scss/components/_form.scss";
+import "./scss/components/_icons.scss";
 
+// CTA button scroll animation
 
-$(".header__button").click(function() {
+$(".header__button").click(function () {
     $('html,body').animate({
-            scrollTop: $(".portfolio").offset().top},
+            scrollTop: $(".portfolio").offset().top
+        },
         'slow');
+});
+
+// images on icons
+$(function () {
+
+    $('.info__icon--facebook').after('<img src="../images/icon-fb.png"  class="info__icon--facebook"/>');
+
+});
+
+$(function () {
+
+    $('.info__icon--instagram').after('<img src="../images/icon-insta.png"  class="info__icon--instagram"/>');
+
 });
