e1eb1588904e4568732fe893df7fa6f08b7499e8
AHerod <metalvege@gmail.com>
2018-12-11 23:38:07 +0100

    AJAX form submission

--- js/app.js
+++ js/app.js
@@ -63,4 +63,41 @@ $(function () {
     });
 });
 
+//form submission
+$(function() {
+    var form = $('#ajax-contact');
 
+    var formMessages = $('#form-messages');
+
+    $(form).submit(function(event) {
+        event.preventDefault();
+
+    });
+    var formData = $(form).serialize();
+    $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+    })
+        .done(function(response) {
+            $(formMessages).removeClass('messageForm__validation--fail');
+            $(formMessages).addClass('messageForm__validation--success');
+
+            $(formMessages).text(response);
+
+            // Clear the form.
+            $('#name').val('');
+            $('#email').val('');
+            $('#message').val('');
+        })
+        .fail(function(data) {
+            $(formMessages).removeClass('messageForm__validation--success');
+            $(formMessages).addClass('messageForm__validation--fail');
+
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Podczas przesyłania wiadomości wystąpił błąd');
+            }
+        });
+});
